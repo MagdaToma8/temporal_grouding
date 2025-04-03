6b8841cf5e704f2ae621cc52542749911c09c027
bulatkh <bulatkhaert@gmail.com>
2025-04-03 14:57:09 +0200

    Update scripts

--- scripts/train_summarizer_blip2.sh
+++ scripts/train_summarizer_blip2.sh
@@ -1,6 +1,7 @@
 python -m src.train_summarizer \
---data_config configs/flickr30k/data_summarizer_blip2.yaml \
+--dataset coco \
+--data_config configs/coco/data_summarizer_blip2.yaml \
 --model_family blip2-embeddings \
 --model_id Salesforce/blip2-itm-vit-g \
---experiment_config ./configs/flickr30k/blip2_local_summarizer.yaml \
---num_workers 8
+--experiment_config ./configs/coco/blip2_local_summarizer_nocaploss.yaml \
+--num_workers 0
